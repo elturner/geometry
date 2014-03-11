e37c0cb69eaf7412bf03a1f70b659781bfa05553
elturner <elturner@eecs.berkeley.edu>
2014-03-11 13:57:15 -0700

    added third-party 3D polygon/box intersection code

---
+++ src/cpp/geometry/poly_intersect/get_polygon_normal.c
@@ -0,0 +1,47 @@
+#include <math.h>
+#include "vec.h"
+#include "pcube.h"
+
+/**
+ * @file get_polygon_normal.c
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ *
+ * @section DESCRIPTION
+ *
+ * Finds normal vector of triangle, authored by Don Hatch &
+ * Melinda (Daniel) Green, January 1994.
+ */
+
+/*
+ *  Calculate a vector perpendicular to a planar polygon.
+ *  If the polygon is non-planar, a "best fit" plane will be used.
+ *  The polygon may be concave or even self-intersecting,
+ *  but it should have nonzero area or the result will be a zero vector
+ *  (e.g. the "bowtie" quad).
+ *  The length of vector will be twice the area of the polygon.
+ *  NOTE:  This algorithm gives the same answer as Newell's method
+ *  (see Graphics Gems III) but is slightly more efficient than Newell's
+ *  for triangles and quads (slightly less efficient for higher polygons).
+ */
+static real *
+get_polygon_normal(real normal[3],
+		   int nverts, const real verts[/* nverts */][3])
+{
+    int i;
+    real tothis[3], toprev[3], cross[3];
+
+    /*
+     * Triangulate the polygon and sum up the nverts-2 triangle normals.
+     */
+    ZEROVEC3(normal);
+    VMV3(toprev, verts[1], verts[0]);  	/* 3 subtracts */
+    for (i = 2; i <= nverts-1; ++i) {   /* n-2 times... */
+	VMV3(tothis, verts[i], verts[0]);    /* 3 subtracts */
+	VXV3(cross, toprev, tothis);         /* 3 subtracts, 6 multiplies */
+	VPV3(normal, normal, cross);         /* 3 adds */
+	SET3(toprev, tothis);
+    }
+    return normal;
+}
+
+// TODO
---
+++ src/cpp/geometry/poly_intersect/get_polygon_normal.h
@@ -0,0 +1,31 @@
+#ifndef POLY_INTERSECT_H
+#define POLY_INTERSECT_H
+
+/**
+ * @file get_polygon_normal.h
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ *
+ * @section DESCRIPTION
+ *
+ * Finds normal vector of triangle, authored by Don Hatch &
+ * Melinda (Daniel) Green, January 1994.
+ */
+
+#include "vec.h"
+#include "pcube.h"
+
+/*
+ *  Calculate a vector perpendicular to a planar polygon.
+ *  If the polygon is non-planar, a "best fit" plane will be used.
+ *  The polygon may be concave or even self-intersecting,
+ *  but it should have nonzero area or the result will be a zero vector
+ *  (e.g. the "bowtie" quad).
+ *  The length of vector will be twice the area of the polygon.
+ *  NOTE:  This algorithm gives the same answer as Newell's method
+ *  (see Graphics Gems III) but is slightly more efficient than Newell's
+ *  for triangles and quads (slightly less efficient for higher polygons).
+ */
+static real* get_polygon_normal(real normal[3],
+		   int nverts, const real verts[/* nverts */][3]);
+
+#endif
