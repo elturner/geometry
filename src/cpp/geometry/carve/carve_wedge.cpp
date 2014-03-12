5ab2154dd55b7be45ecb7a5f396cf3dfed033cdc
elturner <elturner@eecs.berkeley.edu>
2014-03-12 15:03:56 -0700

    added carve_wedge_t class, which defines geometry between adjacent scan points

---
+++ src/cpp/geometry/carve/carve_wedge.cpp
@@ -0,0 +1,278 @@
+#include "carve_wedge.h"
+#include <geometry/carve/gaussian/carve_map.h>
+#include <geometry/octree/octdata.h>
+#include <geometry/poly_intersect/pcube.h>
+#include <geometry/poly_intersect/get_polygon_normal.h>
+#include <stdlib.h>
+#include <iostream>
+#include <Eigen/Dense>
+
+/**
+ * @file carve_wedge.cpp
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ *
+ * @section DESCRIPTION
+ *
+ * The carve_wedge_t class is used to represent a volume in 3D space,
+ * and correlate the continuous mapping functions from a carve_map_t
+ * to this volume, which allows a carve_map_t to be expressed over
+ * a finite volume where it has the most impact.
+ *
+ * The wedge is formed from four carve_map_t objects, which all
+ * contribute to the mapping inside the volume of the wedge.
+ *
+ * This wedge is also used as a shape that can intersect with octrees,
+ * allowing for efficient insertion of carve maps into octrees.
+ *
+ * This file requires the Eigen framework.
+ */
+
+using namespace std;
+using namespace Eigen;
+
+/* the following defines are used in this code */
+
+/* each wedge is represented by a set of surface triangles, which
+ * are used to test intersections with the cubes that represent the
+ * nodes of an octree.  A wedge is oriented as the following:
+ *
+ *            scanpoint #i+1, maps[3]                             
+ *                                                                 
+ *              5         scanpoint #i+1                           
+ *              /--_    2       maps[1]                               
+ *             /  - --_.                                              
+ *            /    -  / -                                              
+ *           /      -/   -                                             
+ * next     /       /-    -                                            
+ * sensor  /       /  -    -  scanpoint #i                             
+ * pos    /       /    - 4  -       maps[2]                            
+ *     3 .------ / ------.   -                                          
+ *        -     /          .  -                                        
+ *         -   /             . -                                       
+ *          - /                .-                                      
+ *            .--------------------                                    
+ *           0                      1                             
+ *         current                     scanpoint #i               
+ *         sensor                      maps[0]                    
+ *         position                                               
+ *                                                                
+ */
+
+#define NUM_TRIANGLES_PER_WEDGE 10 /* number of triangles to test */
+#define NUM_EDGES_PER_WEDGE 15 /* number of edges to test */
+
+#define NUM_VERTS_PER_TRI 3 /* basic geometry */
+
+/* the following define the triangle indices that
+ * represent the polygons in this wedge to test for
+ * intersections. These indices are into the columns
+ * of this->verts, and index starting at 0. */
+static const unsigned int tri_inds[NUM_TRIANGLES_PER_WEDGE][3] = {
+				{0, 1, 2}, /* 0: intra-frame current */
+				{3, 5, 4}, /* 1: intra-frame next */
+				{0, 3, 4}, /* 2: intra-scan #i, half */
+				{0, 4, 1}, /* 3: intra-scan #i, half */
+				{0, 2, 5}, /* 4: intra-scan #i+1, half */
+				{0, 5, 3}, /* 5: intra-scan #i+1, half */
+				{1, 4, 5}, /* 6: intra-points, half */
+				{1, 5, 2}, /* 7: intra-points, half */
+				{0, 1, 5}, /* 8: cross-support, half */
+				{3, 4, 2}  /* 9: corss-support, half */};
+
+/* the following define all edges of the shape, where each pair
+ * represents two vertex indices, indexed from zero. */
+static const unsigned int edge_inds[NUM_EDGES_PER_WEDGE][2] = {
+				{0, 1}, /* intra current frame */
+				{1, 2},
+				{2, 0},
+				{3, 4}, /* intra next frame */
+				{4, 5},
+				{5, 3},
+				{0, 3}, /* parallel between frames */
+				{1, 4},
+				{2, 5},
+				{0, 4}, /* cross-frame diagonals, #i */
+				{3, 1},
+				{0, 5}, /* cross-frame diagonals, #i+1 */
+				{3, 2},
+				{1, 5}, /* cross-diagonals, scanpoints */
+				{4, 2}};
+
+/* function implementations */
+
+/*--------------*/
+/* constructors */
+/*--------------*/
+
+carve_wedge_t::carve_wedge_t()
+{
+	unsigned int i;
+
+	/* initialize empty maps */
+	for(i = 0; i < NUM_MAPS_PER_WEDGE; i++)
+		this->maps[i] = NULL;
+}
+
+void carve_wedge_t::init(carve_map_t* a1, carve_map_t* a2,
+                         carve_map_t* b1, carve_map_t* b2,
+                         double nb)
+{
+	Vector3d u, as, bs, a1p, a2p, b1p, b2p;
+	double s;
+
+	/* save these maps to this object */
+	this->maps[0] = a1;
+	this->maps[1] = a2;
+	this->maps[2] = b1;
+	this->maps[3] = b2;
+
+	/* define vertex positions for this wedge.  Note that we
+	 * want the scanpoint vertex positions to be spread farther 
+	 * than the mean positions for each carve map, so that the carving
+	 * can apply to the full spread of each distribution. */
+	
+	/* get mean of points */
+	a1->get_sensor_mean(as);
+	a1->get_scanpoint_mean(a1p);
+	a2->get_scanpoint_mean(a2p);
+	b1->get_sensor_mean(bs);
+	b1->get_scanpoint_mean(b1p);
+	b2->get_scanpoint_mean(b2p);
+
+	/* find position of vertex #0 */
+	this->verts[0] = as;
+	
+	/* find position of vertex #1 */
+	u = (a1p - as);
+	u.normalize(); /* unit vector in direction away from shape */
+	s = a1->get_scanpoint_var(); /* variance in this direction */
+	this->verts[1] = a1p + nb*sqrt(s)*u;
+
+	/* find position of vertex #2 */
+	u = (a2p - as);
+	u.normalize(); /* unit vector in direction away from shape */
+	s = a2->get_scanpoint_var(); /* variance in this direction */
+	this->verts[2] = a2p + nb*sqrt(s)*u;
+
+	/* find position of vertex #3 */
+	this->verts[3] = bs;
+
+	/* find position of vertex #4 */
+	u = (b1p - bs);
+	u.normalize(); /* unit vector in direction away from shape */
+	s = b1->get_scanpoint_var(); /* variance in this direction */
+	this->verts[4] = b1p + nb*sqrt(s)*u;
+
+	/* find position of vertex #5 */
+	u = (b2p - bs);
+	u.normalize(); /* unit vector in direction away from shape */
+	s = b2->get_scanpoint_var(); /* variance in this direction */
+	this->verts[5] = b2p + nb*sqrt(s)*u;
+}
+		
+/*----------*/
+/* geometry */
+/*----------*/
+
+bool carve_wedge_t::intersects(const Eigen::Vector3d& c, double hw) const
+{
+	double vs[NUM_VERTICES_PER_WEDGE][3];
+	double curr_tri[NUM_VERTS_PER_TRI][3];
+	double curr_norm[3];
+	unsigned int i, j, k;
+	double s;
+	int ret;
+
+	/* prepare array of transformed vertices, which are the
+	 * vertices of this wedge in the transform where this box
+	 * is the unit box centered at the origin. */
+	s = 0.5/hw; /* scale factor */
+	for(i = 0; i < NUM_VERTICES_PER_WEDGE; i++)
+		for(j = 0; j < 3; j++) /* iterate over dimensions */
+			vs[i][j] = (this->verts[i](j) - c(j)) * s;
+
+	/* perform a simple vertex intersection test on the box */
+	ret = trivial_vertex_tests(NUM_VERTICES_PER_WEDGE, vs, false);
+	switch(ret)
+	{
+		/* this case indicates that at least one vertex intersects
+		 * the volume of the cube, which is a trivial accept
+		 * for the intersection */
+		case 1: 
+			return true;
+		
+		/* this case indicates that all vertices are off to one
+		 * side of the cube, which prevents the possibility of
+		 * intersection, trivial reject. */
+		case 0:
+			return false;
+
+		/* need to search deeper */
+		default:
+			break;
+	}
+	
+	/* iterate over the edge of the shape, checking
+	 * for box-line intersection */
+	for(i = 0; i < NUM_EDGES_PER_WEDGE; i++)
+		if(segment_intersects_cube(vs[edge_inds[i][0]],
+						vs[edge_inds[i][1]]))
+			return true; /* edge intersects */
+	
+	/* no vertices intersected, and no edges
+	 * intersected, so now we try the triangulation
+	 * of the faces, to check if those polygons
+	 * intersect the cube. */
+	for(i = 0; i < NUM_TRIANGLES_PER_WEDGE; i++)
+	{
+		/* construct array for current triangle */
+		for(j = 0; j < NUM_VERTS_PER_TRI; j++) /* iter over verts */
+			for(k = 0; j < 3; k++) /* iter over dims */
+				curr_tri[j][k] = vs[tri_inds[i][j]][k];
+
+		/* compute normal for this triangle */
+		get_polygon_normal(curr_norm, NUM_VERTS_PER_TRI, curr_tri);
+
+		/* test if this triangle intersects the cube */
+		if(fast_polygon_intersects_cube(NUM_VERTS_PER_TRI,
+					curr_tri, curr_norm, true, true))
+			return true; /* triangle intersects the cube */
+	}
+
+	/* no intersections were found */
+	return false;
+}
+		
+octdata_t* carve_wedge_t::apply(const Eigen::Vector3d& c, double hw,
+                                octdata_t* d) const
+{
+	return d; // TODO
+}
+		
+/*-----------*/
+/* debugging */
+/*-----------*/
+
+void carve_wedge_t::writeobj(std::ostream& os) const
+{
+	unsigned int i;
+
+	/* print comments */
+	os << "# The following shape generated from a carve wedge" << endl;
+
+	/* print all vertices */
+	for(i = 0; i < NUM_VERTICES_PER_WEDGE; i++)
+		os << "v " << this->verts[i](0)
+		   <<  " " << this->verts[i](1)
+		   <<  " " << this->verts[i](2) << endl;
+
+	/* print all triangles, note that wavefront obj
+	 * indexes from 1, and can handle relative indices,
+	 * where the most recent vertex is -1, second-most-recent is -2,
+	 * etc. */
+	for(i = 0; i < NUM_TRIANGLES_PER_WEDGE; i++)
+		os << "f -" << (NUM_VERTICES_PER_WEDGE - tri_inds[i][0])
+		   <<  " -" << (NUM_VERTICES_PER_WEDGE - tri_inds[i][1])
+		   <<  " -" << (NUM_VERTICES_PER_WEDGE - tri_inds[i][2])
+		   << endl;
+}
---
+++ src/cpp/geometry/carve/carve_wedge.h
@@ -0,0 +1,145 @@
+#ifndef CARVE_WEDGE_H
+#define CARVE_WEDGE_H
+
+/**
+ * @file carve_wedge.h
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ *
+ * @section DESCRIPTION
+ *
+ * The carve_wedge_t class is used to represent a volume in 3D space,
+ * and correlate the continuous mapping functions from a carve_map_t
+ * to this volume, which allows a carve_map_t to be expressed over
+ * a finite volume where it has the most impact.
+ *
+ * The wedge is formed from four carve_map_t objects, which all
+ * contribute to the mapping inside the volume of the wedge.
+ *
+ * This wedge is also used as a shape that can intersect with octrees,
+ * allowing for efficient insertion of carve maps into octrees.
+ *
+ * This file requires the Eigen framework.
+ */
+
+#include <geometry/carve/gaussian/carve_map.h>
+#include <geometry/octree/octdata.h>
+#include <iostream>
+#include <Eigen/Dense>
+
+/* the following defines are used for this class */
+#define NUM_MAPS_PER_WEDGE 4 /* number of scanpoints to make a wedge */
+#define NUM_VERTICES_PER_WEDGE 6 /* number of vertices in polyhedron */
+
+/**
+ * This class represents a 3D shape formed by four scan points
+ *
+ * This shape originates from four scan points, two neighboring scans
+ * from one frame, then the same indices in the successor frame.
+ */
+class carve_wedge_t
+{
+	/* parameters */
+	private:
+
+		/* the following are pointers to carve maps to
+		 * use to define this shape.  This object is NOT
+		 * responsible for the memory allocation of these
+		 * maps, which is handled elsewhere. */
+		carve_map_t* maps[NUM_MAPS_PER_WEDGE];
+
+		/* the following define the world-coordinate vertices
+		 * of this wedge.  These vertices are placed outside
+		 * the mean-positions of the scans, so that the wedge
+		 * includes not only the mean positions but an extra
+		 * buffer to carve. */
+		Eigen::Vector3d verts[NUM_VERTICES_PER_WEDGE];
+
+	/* functions */
+	public:
+
+		/*--------------*/
+		/* constructors */
+		/*--------------*/
+
+		/* Since this class contains Eigen structures, we
+		 * need to properly align memory */
+		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+		
+		/**
+		 * Initializes empty wedge
+		 */
+		carve_wedge_t();
+
+		/**
+		 * Initialize this wedge with input carve maps
+		 *
+		 * Given four carve maps, will initialze the shape of
+		 * this wedge.  Note that the wedge will be defined to
+		 * be larger than the mean positions of the originating
+		 * scans.  The buffer width of this increase is proportional
+		 * to the standard deviation of the distributions provided,
+		 * and scaled by the input buffer factor.
+		 *
+		 * @param a1   carve map indicating frame #j, point #i
+		 * @param a2   carve map indicating frame #j, point #i+1
+		 * @param b1   carve map indicating frame #j+1, point #i
+		 * @param b2   carve map indicating frame #j+1, point #i+1
+		 * @param nb   number of standard deviations of buffer
+		 */
+		void init(carve_map_t* a1, carve_map_t* a2,
+		          carve_map_t* b1, carve_map_t* b2,
+			  double nb);
+
+		/*----------*/
+		/* geometry */
+		/*----------*/
+
+		/**
+		 * Checks if this shape intersects a given axis-aligned box
+		 *
+		 * Given the parameters of an Axis-Aligned Bounding Box
+		 * (AABB), will check if this wedge shape intersects the
+		 * AABB.
+		 *
+		 * @param c       The center of the box, in 3D space
+		 * @param hw      Half the width of an edge of the box
+		 *
+		 * @return    Returns true iff this intersects given box
+		 */
+		bool intersects(const Eigen::Vector3d& c, double hw) const;
+
+		/**
+		 * Applies this mapping to the given data element
+		 *
+		 * Given a data element, its location, and size, will
+		 * compute the carve mapping for that location and store
+		 * it in the given data element.  If the given data element
+		 * is null, this function call will allocate a new element
+		 * and return it.  If the input is not null, then it will
+		 * be modified and the same element will be returned.
+		 *
+		 * @param c     The location of this data element in space
+		 * @param hw    The half-width of this data element
+		 * @param d     The data element to modify, can be null
+		 *
+		 * @return    Returns the modified data, or new data object.
+		 */
+		octdata_t* apply(const Eigen::Vector3d& c, double hw,
+		                 octdata_t* d) const;
+
+		/*-----------*/
+		/* debugging */
+		/*-----------*/
+
+		/**
+		 * Will export this wedge to an OBJ file stream
+		 *
+		 * Will write out the triangles and vertices of this
+		 * wedge to a Wavefront OBJ formatted file stream.
+		 *
+		 * @param os   The output stream to write to
+		 */
+		void writeobj(std::ostream& os) const;
+};
+
+#endif
