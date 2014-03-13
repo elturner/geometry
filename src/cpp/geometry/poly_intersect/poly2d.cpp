da845e9b62033c015a3ed874b9ca811951d4a5dc
elturner <elturner@eecs.berkeley.edu>
2014-03-13 16:49:30 -0700

    began working on poly2d library

---
+++ src/cpp/geometry/poly_intersect/poly2d.cpp
@@ -0,0 +1,13 @@
+#include "poly2d.h"
+
+/**
+ * @file poly2d.cpp
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ *
+ * @section DESCRIPTION
+ *
+ * This file implements functions useful for checking intersections
+ * of 2D shape primitives and polygons.
+ */
+
+// TODO left off here
---
+++ src/cpp/geometry/poly_intersect/poly2d.h
@@ -0,0 +1,16 @@
+#ifndef POLY2D_H
+#define POLY2D_H
+
+/**
+ * @file poly2d.h
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ *
+ * @section DESCRIPTION
+ *
+ * This file defines functions useful for checking intersections
+ * of 2D shape primitives and polygons.
+ */
+
+// TODO left off here
+
+#endif
