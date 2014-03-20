ae6c6bd3db624ed20aab5b88ef53d9f725e15bd1
elturner <elturner@eecs.berkeley.edu>
2014-03-20 13:52:33 -0700

    added chunk_exporter_t class

---
+++ src/cpp/geometry/shapes/chunk_exporter.cpp
@@ -0,0 +1,261 @@
+#include "chunk_exporter.h"
+#include <io/carve/chunk_io.h>
+#include <geometry/octree/shape.h>
+#include <geometry/octree/octree.h>
+#include <geometry/octree/octnode.h>
+#include <geometry/octree/octdata.h>
+#include <geometry/shapes/carve_wedge.h>
+#include <util/error_codes.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <Eigen/Dense>
+
+/**
+ * @file chunk_exporter.cpp
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ * @brief Classes to populate and export volume chunks
+ *
+ * @section DESCRIPTION
+ *
+ * This file implements the chunk_exporter_t class, which is a shape
+ * that can be inserted into an octree.  By inserting this shape,
+ * the tree's data elements will be created but not populated, and
+ * the information about which shapes intersect which nodes will
+ * be exported as chunk files.
+ *
+ * This class requires the Eigen framework.
+ */
+
+using namespace std;
+using namespace Eigen;
+using namespace chunk;
+
+/* function implementations */
+
+chunk_exporter_t::chunk_exporter_t()
+{
+	/* initialize default values */
+	this->chunk_map.clear();
+	this->reference_shape    = NULL;
+	this->vals.clear();
+	this->chunklist_filename = "";
+	this->full_chunk_dir     = "";
+	this->rel_chunk_dir      = "";
+}
+
+chunk_exporter_t::~chunk_exporter_t()
+{
+	vector<string> names;
+
+	/* check that everything has been properly closed */
+	if(!(this->chunk_map.empty()))
+	{
+		/* Uh oh!  The user didn't call close! */
+		cerr << "[chunk_exporter_t:~chunk_exporter_t]\t"
+		     << "You forgot to call chunk_exporter_t::close()"
+		     << endl;
+
+		/* call close with what info we have */
+		this->close(0,0,0,-1,names);
+	}
+}
+
+void chunk_exporter_t::set(shape_t* rs, const vector<point_index_t>& v)
+{
+	/* copy over shape */
+	this->reference_shape = rs;
+
+	/* copy over vals */
+	vals.clear();
+	vals.insert(vals.begin(), v.begin(), v.end());
+}
+		
+#define FILE_SEP_CHARS "\\/"
+void chunk_exporter_t::open(const string& clfile, const string& chunk_dir)
+{
+	size_t found;
+
+	/* copy over file names */
+	this->chunklist_filename = clfile;
+	this->rel_chunk_dir = chunk_dir;
+
+	/* determine path to chunk dir from working directory */
+	found = clfile.find_last_of(FILE_SEP_CHARS);
+	if(found == string::npos)
+	{
+		/* in working directory, no work needs to be done */
+		this->full_chunk_dir = chunk_dir;
+		return;
+	}
+	
+	/* prepend path to chunklists directory to the chunk_dir */
+	this->full_chunk_dir = clfile.substr(0, found+1) + chunk_dir;
+	if(this->full_chunk_dir.find_last_of(FILE_SEP_CHARS)
+			!= this->full_chunk_dir.size()-1)
+	{	
+		/* make sure the full chunk directory ends with
+		 * a file separator, so we can easily generate
+		 * the filepaths to chunk files */
+		this->full_chunk_dir.push_back(FILE_SEPERATOR);
+	}
+}
+		
+int chunk_exporter_t::close(const octree_t& tree,
+                            const vector<string>& sensor_names)
+{
+	octnode_t* root;
+	int ret;
+
+	/* get the root of the tree */
+	root = tree.get_root();
+
+	/* call other version of this function */
+	if(root == NULL)
+	{
+		ret = this->close(0,0,0,0,sensor_names);
+		if(ret)
+			return PROPEGATE_ERROR(-1, ret);
+	}
+	else
+	{
+		ret = this->close(root->center(0), root->center(1),
+		            root->center(2), root->halfwidth, sensor_names);
+		if(ret)
+			return PROPEGATE_ERROR(-2, ret);
+	}
+
+	/* success */
+	return 0;
+}
+	
+int chunk_exporter_t::close(double cx, double cy, double cz, double hw,
+                            const vector<string>& sensor_names)
+{
+	chunklist_writer_t outfile;
+	map<octdata_t*, chunk_writer_t*>::iterator mit;
+	vector<string>::const_iterator vit;
+	int ret;
+
+	/* close all open chunk files */
+	for(mit = this->chunk_map.begin();
+			mit != this->chunk_map.end(); mit++)
+		mit->second->close();
+
+	/* prepare the chunklist file for writing */
+	outfile.init(cx, cy, cz, hw, this->rel_chunk_dir,
+	             this->chunk_map.size());
+
+	/* add all sensors */
+	for(vit = sensor_names.begin(); vit != sensor_names.end(); vit++)
+		outfile.add_sensor(*vit);
+
+	/* open the file for writing */
+	ret = outfile.open(this->chunklist_filename);
+	if(ret)
+		return PROPEGATE_ERROR(-1, ret);
+
+	/* write each chunk uuid to file */
+	for(mit = this->chunk_map.begin();
+			mit != this->chunk_map.end(); mit++)
+	{
+		/* write uuid to file */
+		outfile.write(chunk_exporter_t::ptr_to_uuid(mit->first));
+	}
+
+	/* clean up */
+	outfile.close();
+	for(mit = this->chunk_map.begin();
+			mit != this->chunk_map.end(); mit++)
+	{
+		/* free allocated writer */
+		delete (mit->second);
+		mit->second = NULL;
+	}
+	this->chunk_map.clear();
+
+	/* success */
+	return 0;
+}
+
+octdata_t* chunk_exporter_t::apply_to_leaf(const Vector3d& c,
+                                           double hw, octdata_t* d)
+{
+	string filename;
+	unsigned long long uuid;
+	map<octdata_t*, chunk_writer_t*>::iterator mit;
+	pair<map<octdata_t*, chunk_writer_t*>::iterator, bool> ins;
+	vector<point_index_t>::iterator vit;
+	int ret;
+
+	/* check if chunk file already exists for this data */
+	if(d == NULL)
+	{
+		/* prepare a new data object */
+		d = new octdata_t();
+	
+		/* create a chunk file based on this data object */
+		uuid = (unsigned long long) d;
+		filename = this->full_chunk_dir 
+				+ chunk_exporter_t::ptr_to_uuid(mit->first)
+				+ CHUNKFILE_EXTENSION;
+		
+		/* insert into map */
+		ins = this->chunk_map.insert(make_pair<octdata_t*,
+				chunk_writer_t*>(d, new chunk_writer_t()));
+		if(!(ins.second))
+		{
+			/* We've already inserted this data pointer!?!?
+			 * But we just created it!  This means we
+			 * have duplicate memory issues going on.  Bad
+			 * news!  Inform the user */
+			cerr << "[chunk_exporter_t::apply_to_leaf]\t"
+			     << "Found duplicate allocated pointers! "
+			     << "That's bad news!" << endl << endl;
+		}
+
+		/* keep the iterator */
+		mit = ins.first;
+
+		/* prepare file for writing */
+		mit->second->init(uuid, c(0), c(1), c(2), hw);
+		ret = mit->second->open(filename);
+		if(ret)
+		{
+			/* unable to open file! */
+			cerr << "[chunk_exporter_t::apply_to_leaf]\t"
+			     << "Error " << ret << ": Unable to write "
+			     << "to chunk file" << endl << endl;
+			return d;
+		}
+	}
+	else
+	{
+		/* find this data pointer in the map */
+		mit = this->chunk_map.find(d);
+		if(mit == this->chunk_map.end())
+		{
+			/* Oh uh.  Any data pointer should have already
+			 * been inserted in the map when it was created.
+			 * This is likely a symptom of misuse of this
+			 * function.  But for now, add this pointer
+			 * and continue. */
+			cerr << "[chunk_exporter_t::apply_to_leaf]\t"
+			     << "WARNING: need to insert pre-existing data"
+			     << endl << endl;
+			ins = this->chunk_map.insert(make_pair<octdata_t*,
+				chunk_writer_t*>(d, new chunk_writer_t()));
+			mit = ins.first;
+		}
+	}
+
+	/* add all values to file */
+	for(vit = this->vals.begin(); vit != this->vals.end(); vit++)
+		mit->second->write(*vit);
+
+	/* success */
+	return d;
+}
+
---
+++ src/cpp/geometry/shapes/chunk_exporter.h
@@ -0,0 +1,299 @@
+#ifndef CHUNK_EXPORTER_H
+#define CHUNK_EXPORTER_H
+
+/**
+ * @file chunk_exporter.h
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ * @brief Classes to populate and export volume chunks
+ *
+ * @section DESCRIPTION
+ *
+ * This file defines the chunk_exporter_t class, which is a shape
+ * that can be inserted into an octree.  By inserting this shape,
+ * the tree's data elements will be created but not populated, and
+ * the information about which shapes intersect which nodes will
+ * be exported as chunk files.
+ *
+ * This class requires the Eigen framework.
+ */
+
+#include <io/carve/chunk_io.h>
+#include <geometry/octree/shape.h>
+#include <geometry/octree/octree.h>
+#include <geometry/octree/octnode.h>
+#include <geometry/octree/octdata.h>
+#include <geometry/shapes/carve_wedge.h>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <Eigen/Dense>
+
+/**
+ * The chunk_exporter_t class is used to chunk out octree information
+ *
+ * An object of this class can be given a shape and a set of point indices,
+ * and by inserting this object into an octree, any leafs that are
+ * intersected by the given shape will be exported as chunks with the
+ * specified point indices as chunk data.
+ */
+class chunk_exporter_t : public shape_t
+{
+	/* parameters */
+	private:
+
+		/**
+		 * The list of exported chunks
+		 *
+		 * All observed leafs are exported as chunks.  Each leaf
+		 * is uniquely identified by the address of its data. This
+		 * list of file descriptors is kept so that one
+		 * chunk file is generated for each leaf that contains
+		 * all intersection information, over many inserted
+		 * shapes.  These files will be maintained until
+		 * this object is closed.
+		 */
+		std::map<octdata_t*, chunk::chunk_writer_t*> chunk_map;
+
+		/**
+		 * This value represents the current shape to intersect
+		 *
+		 * This shape will be used to intersect into given octrees.
+		 * Inserting this chunk_exporter_t into an octree will
+		 * intersect the same nodes as if this shape was inserted.
+		 *
+		 * This object should be allocated elsewhere.  This class
+		 * is not responsible for memory management of reference
+		 * shapes.
+		 */
+		shape_t* reference_shape;
+
+		/**
+		 * This list indicates what values to export to chunks
+		 *
+		 * When this chunk_exporter_t is inserted into an octree,
+		 * then every leaf node that is intersected will be written
+		 * to a chunk file, and these values will be added to each
+		 * of those chunks.
+		 */
+		std::vector<chunk::point_index_t> vals;
+
+		/**
+		 * Where to store the final .chunklist file
+		 */
+		std::string chunklist_filename;
+
+		/**
+		 * The directory to store the .chunk files
+		 */
+		std::string full_chunk_dir; /* relative to working dir */
+		std::string rel_chunk_dir; /* relative to chunklist file */
+
+	/* functions */
+	public:
+
+		/*--------------*/
+		/* constructors */
+		/*--------------*/
+
+		/**
+		 * Initializes empty object
+		 */
+		chunk_exporter_t();
+
+		/**
+		 * Frees all memory and resources
+		 */
+		~chunk_exporter_t();
+
+		/*-----------*/
+		/* accessors */
+		/*-----------*/
+
+		/**
+		 * Sets the reference shape and point values
+		 *
+		 * This function will set the reference shape to use.
+		 * After this call, and calls to the overloaded functions
+		 * from the shape_t class will return the same values as
+		 * if they were called to rs.
+		 *
+		 * The list v indicates the list of values to insert into
+		 * chunk files that are intersected by this shape.
+		 *
+		 * @param rs   The reference shape to use
+		 * @param v    The chunk values to export for this shape
+		 */
+		void set(shape_t* rs,
+		         const std::vector<chunk::point_index_t>& v);
+
+		/*-----*/
+		/* i/o */
+		/*-----*/
+
+		/**
+		 * Opens a .chunklist file and prepares chunk generation
+		 *
+		 * This function must be called before performing any
+		 * inserts with this object.  This function specifies the
+		 * location of the .chunklist and corresponding .chunk
+		 * files.
+		 *
+		 * When specifying the relative directory for the .chunk
+		 * files, note that this is relative to the location
+		 * of the .chunklist file, so if:
+		 *
+		 * clfile = "foo/bar/list.chunklist"
+		 * chunk_dir = "baz"
+		 *
+		 * then the chunk files will be stored in:
+		 *
+		 * foo/bar/baz/<chunkname>.chunk
+		 *
+		 * @param clfile     Where to write the chunklist file
+		 * @param chunk_dir  The relative directory to store chunks
+		 *
+		 * @return   Returns zero on success, non-zero on failure.
+		 */
+		void open(const std::string& clfile,
+		         const std::string& chunk_dir);
+
+		/**
+		 * Will generate a .chunklist file and close all chunks.
+		 *
+		 * Calling this function will close all file streams to
+		 * chunks that are currently active, and will generate
+		 * a corresponding .chunklist file for these chunks.
+		 *
+		 * After this call, you must make another call to open()
+		 * to use this object again.
+		 *
+		 * Note that the tree provided as an argument must be
+		 * the same tree that was used for all inserts.  The
+		 * list of sensors must correspond with the sensors
+		 * used when calling the set() function.
+		 *
+		 * @param tree          The tree that has been chunked
+		 * @param sensor_names  The sensors that were used
+		 *
+		 * @return    Returns zero on success, non-zero on failure.
+		 */
+		int close(const octree_t& tree,
+		          const std::vector<std::string>& sensor_names); 
+
+		/**
+		 * Overloaded close() function.
+		 *
+		 * @param cx             The x-coordinate of tree center
+		 * @param cy             The y-coordinate of tree center
+		 * @param cz             The z-coordinate of tree center
+		 * @param hw             The halfwidth of tree
+		 * @param sensor_names   The sensors that were used
+		 *
+		 * @return    Returns zero on success, non-zero on failure.
+		 */
+		int close(double cx, double cy, double cz, double hw,
+		          const std::vector<std::string>& sensor_names);
+
+		/*----------------------------*/
+		/* overloaded shape functions */
+		/*----------------------------*/
+
+		/**
+		 * Specifies the number of vertices for this shape.
+		 *
+		 * @return   Returns the number of shape vertices
+		 */
+		inline unsigned int num_verts() const
+		{
+			/* just use the reference shape */
+			if(this->reference_shape == NULL)
+				return 0;
+			return this->reference_shape->num_verts();
+		};
+
+		/**
+		 * Retrieves the i'th vertex
+		 *
+		 * Will return the position of the specified vertex.  Note
+		 * that only indices within [0, num_verts() ) are valid.
+		 *
+		 * @param i   The index of the vertex to retrieve
+		 * 
+		 * @return    Returns the position of the i'th vertex
+		 */
+		inline Eigen::Vector3d get_vertex(unsigned int i) const
+		{
+			Eigen::Vector3d x;
+
+			/* use the reference shape */
+			if(this->reference_shape == NULL)
+				return x;
+			return this->reference_shape->get_vertex(i);
+		};
+
+		/**
+		 * Check for intersection against an AABB
+		 *
+		 * Checks if the reference shape of this exporter intersects
+		 * the given parameters for an Axis-Aligned Bounding Box.
+		 *
+		 * @param c   The center of the box
+		 * @param hw  The half-width of the box
+		 *
+		 * @return    Returns true iff an intersection occurs.
+		 */
+		inline bool intersects(const Eigen::Vector3d& c,
+		                       double hw) const
+		{
+			/* use reference shape */
+			if(this->reference_shape == NULL)
+				return false; /* no shape -> no intersect */
+			return this->reference_shape->intersects(c,hw);
+		};
+
+		/**
+		 * Will update chunk file for this leaf
+		 *
+		 * By calling this, will generate/append the current
+		 * chunk values to the chunk file associated with this
+		 * leaf.  The file is hashed by the address of the leaf's
+		 * data. If the leaf has no data, then a data object will
+		 * be allocated to the leaf and returned.
+		 *
+		 * @param c   The center position of the leaf
+		 * @param hw  The half-width of the leaf geometry
+		 * @param d   The current data object for the leaf
+		 *
+		 * @return    Returns the leaf's data object, which may be
+		 *            newly allocated by this function.  If the
+		 *            provided data is not null, then d will be
+		 *            returned.
+		 */
+		octdata_t* apply_to_leaf(const Eigen::Vector3d& c,
+		                         double hw, octdata_t* d);
+
+	/* helper functions */
+	private:
+
+		/**
+		 * Converts a pointer to a uuid string
+		 *
+		 * @param p   The pointer to convert
+		 *
+		 * @return    A uuid string representation of pointer
+		 */
+		static inline std::string ptr_to_uuid(void* p)
+		{
+			std::stringstream ss;
+
+			/* convert pointer to string */
+			ss.str("");
+			ss << p;
+			return ss.str();
+		};
+
+};
+
+
+#endif
