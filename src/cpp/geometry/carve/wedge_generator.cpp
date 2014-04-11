1230ac5b24495e53af5efbe5d6b42d0f9deee15e
elturner <elturner@eecs.berkeley.edu>
2014-04-11 16:17:22 -0700

    added wedge generator code to the repo, which converts fss files into wedge files.  This does much of the same processing that the random_carver_t used to do

---
+++ src/cpp/geometry/carve/wedge_generator.cpp
@@ -0,0 +1,242 @@
+#include "wedge_generator.h"
+#include <io/data/fss/fss_io.h>
+#include <io/carve/wedge_io.h>
+#include <timestamp/sync_xml.h>
+#include <geometry/system_path.h>
+#include <geometry/carve/frame_model.h>
+#include <geometry/carve/gaussian/scan_model.h>
+#include <util/progress_bar.h>
+#include <util/error_codes.h>
+#include <util/tictoc.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+/**
+ * @file wedge_generator.cpp
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ * @brief  Computes all wedges and writes them to file
+ *
+ * @section DESCRIPTION
+ *
+ * This file contains a class that will perform all probabilistic
+ * computations for a set of scanners, in order to compute all
+ * wedges from these scanners.  These wedges can then we exported
+ * to a binary file.
+ */
+
+using namespace std;
+
+/* function implementations */
+
+int wedge_generator_t::init(const string& madfile,
+                            const string& confile,
+                            const string& tsfile,
+                            double dcu, double carvebuf)
+{
+	int ret;
+
+	/* initialize the structures */
+	ret = this->path.readmad(madfile);
+	if(ret)
+	{
+		/* report error to user */
+		cerr << "[wedge_generator_t::init]\tUnable to initialize"
+		     << " path of system, Error " << ret << endl;
+		return PROPEGATE_ERROR(-1, ret); /* cannot read file */
+	}
+	ret = this->path.parse_hardware_config(confile);
+	if(ret)
+	{
+		/* report error to user */
+		cerr << "[wedge_generator_t::init]\tUnable to initialize"
+		     << " system hardware config, Error " << ret << endl;
+		return PROPEGATE_ERROR(-2, ret); /* cannot read file */
+	}
+	if(!this->timesync.read(tsfile))
+	{
+		/* report error to user */
+		cerr << "[wedge_generator_t::init]\tUnable to parse the "
+		     << "time synchronization output xml file: "
+		     << tsfile << endl;
+		return PROPEGATE_ERROR(-3, ret);
+	}
+
+	/* initialize octree and algorithm parameters */
+	this->carving_buffer = carvebuf;
+	this->default_clock_uncertainty = dcu;
+
+	/* success */
+	return 0;
+}
+		
+int wedge_generator_t::process(const vector<string>& fssfiles,
+                               const string& wedgefile) const
+{
+	progress_bar_t progbar;
+	wedge::writer_t outfile;
+	fss::reader_t infile;
+	fss::frame_t inframe;
+	scan_model_t model;
+	frame_model_t curr_frame, prev_frame;
+	string label;
+	tictoc_t clk;
+	size_t i, si, n, num_sensors;
+	int ret;
+
+	/* prepare output file */
+	ret = outfile.open(wedgefile);
+	if(ret)
+	{
+		ret = PROPEGATE_ERROR(-1, ret);
+		cerr << "[wedge_generator_t::process]\tERROR " << ret
+		     << ": Could not open output file for writing:"
+		     << wedgefile << endl << endl;
+		return ret;
+	}
+
+	/* iterate over scan files */
+	num_sensors = fssfiles.size();
+	for(si = 0; si < num_sensors; si++)
+	{
+		/* read in scan file */
+		tic(clk);
+		infile.set_correct_for_bias(true); /* correct bias */
+		infile.set_convert_to_meters(true); /* units: meters */
+		ret = infile.open(fssfiles[si]);
+		if(ret)
+		{
+			/* unable to parse, report to user */
+			ret = PROPEGATE_ERROR(-2, ret);
+			infile.close();
+			outfile.close();
+			cerr << "[wedge_generator_t::process]\t"
+			     << "Error " << ret
+			     << ": Unable to parse input scan file "
+			     << fssfiles[si] << endl;
+			return ret;
+		}
+
+		/* prepare noisy model for this scanner */
+		ret = model.set_sensor(infile.scanner_name(), 
+	        	this->get_clock_uncertainty_for_sensor(
+				infile.scanner_name()), this->path);
+		if(ret)
+		{
+			/* not a recognized sensor */
+			ret = PROPEGATE_ERROR(-3, ret);
+			infile.close();
+			outfile.close();
+			cerr << "[wedge_generator_t::process]\t"
+			     << "Error " << ret
+			     << ": Unable to recognize sensor \""
+			     << infile.scanner_name() << "\"" << endl;
+			return ret;
+		}
+		label = "Parsing " + infile.scanner_name();
+		toc(clk, label.c_str());
+
+		/* iterate through scans */
+		tic(clk);
+		progbar.set_name(infile.scanner_name());
+		n = infile.num_frames();
+		for(i = 0; i < n; i++)
+		{
+			/* inform user of progress */
+			progbar.update(i, n);
+		
+			/* parse current frame */
+			ret = infile.get(inframe, i);
+			if(ret)
+			{
+				/* error occurred reading frame, 
+				 * inform user */
+				progbar.clear();
+				infile.close();
+				outfile.close();
+				ret = PROPEGATE_ERROR(-4, ret);
+				cerr << "[wedge_generator_t::process]\t"
+				     << "Unable to parse frame #"
+				     << i << ", Error "
+				     << ret << endl;
+				return ret;
+			}
+		
+			/* compute carving model for this frame */
+			ret = curr_frame.init(inframe, model, this->path);
+			if(ret)
+			{
+				/* error occurred computing 
+				 * frame parameters */
+				progbar.clear();
+				infile.close();
+				outfile.close();
+				ret = PROPEGATE_ERROR(-5, ret);
+				cerr << "[wedge_generator_t::process]\t"
+				     << "Unable to compute frame #" 
+				     << i << ", Error "
+				     << ret << endl;
+				return ret;
+			}
+
+			/* only proceed from here if we have two frame's
+			 * worth of data, so we can interpolate the
+			 * distributions between them and carve the
+			 * corresponding volume. */
+			if(i == 0)
+			{
+				/* prepare for the next frame */
+				curr_frame.swap(prev_frame);
+				continue;
+			}
+
+			/* export all this frame's wedges to file */
+			ret = prev_frame.serialize_wedges(outfile,
+					curr_frame, this->carving_buffer);
+			if(ret <= 0)
+			{
+				/* an error occurred */
+				progbar.clear();
+				infile.close();
+				outfile.close();
+				ret = PROPEGATE_ERROR(-6, ret);
+				cerr << "[wedge_generator_t::process]\t"
+				     << "Error " << ret << ": Unable to "
+				     << "serialize frame #" << (i-1)
+				     << endl << endl;
+				return ret;
+			}
+
+			/* prepare for the next frame */
+			curr_frame.swap(prev_frame);
+		}
+
+		/* inform user that processing is finished */
+		progbar.clear();
+		infile.close();
+		label = "Generating wedges for " + infile.scanner_name();
+		toc(clk, label.c_str());
+	}
+
+	/* clean up */
+	outfile.close();
+
+	/* success */
+	return 0;
+}
+
+double wedge_generator_t::get_clock_uncertainty_for_sensor(
+                        const string& sensor_name) const
+{
+	FitParams clk;
+
+	/* get the timesync parameters for this sensor */
+	clk = this->timesync.get(sensor_name);
+
+	/* check if std. dev. is valid */
+	if(clk.stddev < 0)
+		return this->default_clock_uncertainty;
+
+	/* return this sensor's specific uncertainty */
+	return clk.stddev;
+}
---
+++ src/cpp/geometry/carve/wedge_generator.h
@@ -0,0 +1,106 @@
+#ifndef WEDGE_GENERATOR_H
+#define WEDGE_GENERATOR_H
+
+/**
+ * @file wedge_generator.h
+ * @author Eric Turner <elturner@eecs.berkeley.edu>
+ * @brief  Computes all wedges and writes them to file
+ *
+ * @section DESCRIPTION
+ *
+ * This file contains a class that will perform all probabilistic
+ * computations for a set of scanners, in order to compute all
+ * wedges from these scanners.  These wedges can then we exported
+ * to a binary file.
+ */
+
+#include <timestamp/sync_xml.h>
+#include <geometry/system_path.h>
+#include <string>
+#include <vector>
+
+/**
+ * The wedge_generator_t class computes all wedges for a set of sensors
+ */
+class wedge_generator_t
+{
+	/* parameters */
+	private:
+
+		/* The system path */
+		system_path_t path;
+	
+		/* The clock error represents the uncertainty (std. dev.)
+		 * of the system clock when timestamping hardware sensors.
+		 * It is expressed in units of seconds. This error can
+		 * be different for different sensors, and is described
+		 * in the time synchronization output file. */
+		SyncXml timesync;
+
+		/* if unable to compute the timestamp uncertainty for a
+		 * particular sensor, use this value */
+		double default_clock_uncertainty;
+
+		/* the carving buffer, in units of standard deviations,
+		 * dictates how far past each point will be carved. A
+		 * non-zero buffer allows for the exterior-mapped portions
+		 * of the probability distributions to be explicity
+		 * represented in the octree. */
+		double carving_buffer;
+	
+	/* functions */
+	public:
+		
+		/**
+		 * Initializes this object with specified data sources
+		 *
+		 * Given localization information and processing parameters,
+		 * this function call will prepare this object to perform
+		 * compute probability distributions for each scan frame.
+		 *
+		 * @param madfile   The path file for this dataset
+		 * @param confile   The xml hardware config file
+		 * @param tsfile    The time synchronization output xml file
+		 * @param dcu       The default clock uncertainty
+		 * @param carvebuf  The carving buffer, units of std. dev.
+		 *
+		 * @return     Returns zero on success, non-zero on failure.
+		 */
+		int init(const std::string& madfile,
+		         const std::string& confile,
+		         const std::string& tsfile,
+		         double dcu, double carvebuf);
+
+		/**
+		 * Computes and exports all wedges
+		 *
+		 * Given a list of fss files, will compute all wedge
+		 * distributions from the scan frames in these files,
+		 * and will export these wedge distributions to disk.
+		 *
+		 * @param fssfiles     The list of fss files to analyze
+		 * @param wedgefile    Where to write the wedge file
+		 *
+		 * @return    Returns zero on success, non-zero on failure
+		 */
+		int process(const std::vector<std::string>& fssfiles,
+		            const std::string& wedgefile) const;
+	
+	/* helper functions */
+	private:
+
+		/**
+		 * Gets the timestamp uncertainty for a specific sensor
+		 *
+		 * Given the sensor name, will look up the timestamp
+		 * uncertainty for that sensor, and return the desired
+		 * value.
+		 *
+		 * @return   Returns the std. dev. of the clock error for
+		 *           the specified sensor.
+		 */
+		double get_clock_uncertainty_for_sensor(
+				const std::string& sensor_name) const;
+};
+
+#endif
