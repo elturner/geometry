a62534cd7abee7fb699a220167e435914e5874fe
elturner <elturner@eecs.berkeley.edu>
2014-05-19 16:25:34 -0700

    added matlab io functions for filetype .noisypath

---
+++ src/matlab/path/convert_mad_to_noisypath.m
@@ -0,0 +1,85 @@
+function [] = convert_mad_to_noisypath(madfile, noisypathfile, err_T, err_R)
+	% convert_mad_to_noisypath(madfile, noisypathfile, err_T, err_R)
+	%
+	%	Will generate a .noisypath file from a given .mad file
+	%	by assuming every pose has uniform translation error err_T
+	%	and uniform rotational error err_R.
+	%
+	% arguments:
+	%
+	%	madfile -	The path to the .mad file to use
+	%	noisypathfile -	The output .noisypath file
+	%	err_T -		The standard-deviation to use for position
+	%	err_R -		The standard-deviation to use for rotation
+	%
+
+	% use libraries
+	addpath('../math/');
+	
+	% read the mad file
+	[poses, timestamps, zupts] = readMAD(madfile);
+
+	% prepare output file
+	fid = fopen(noisypathfile, 'wb');
+	if(fid < 0)
+		error(['Unable to open file for writing: ', noisypathfile]);
+	end
+
+	% get sizes of lists
+	num_poses = size(poses, 2);
+	num_zupts = size(zupts, 1);
+
+	% write header information to file
+	fwrite(fid, ['noisypath',0], 'char');
+	fwrite(fid, num_zupts, 'uint32');
+	fwrite(fid, num_poses, 'uint32');
+
+	% write zupt information to file
+	for i = 1:num_zupts
+		fwrite(fid, zupts(i,1), 'double');
+		fwrite(fid, zupts(i,2), 'double');
+	end
+
+	% write pose information to file
+	var_T = err_T * err_T;
+	var_R = err_R * err_R;
+	for i = 1:num_poses
+		
+		% write timestamp
+		fwrite(fid, timestamps(i), 'double');
+
+		% write position information, units of meters
+		fwrite(fid, poses(1,i), 'double'); % x
+		fwrite(fid, poses(2,i), 'double'); % y
+		fwrite(fid, poses(3,i), 'double'); % z
+
+		% write the covariance matrix.  Note that we're making
+		% up this data given the input.
+		fwrite(fid, var_T, 'double'); % Cov(X,X)
+		fwrite(fid, 0,     'double'); % Cov(X,Y)
+		fwrite(fid, 0,     'double'); % Cov(X,Z)
+		fwrite(fid, var_T, 'double'); % Cov(Y,Y)
+		fwrite(fid, 0,     'double'); % Cov(Y,Z)
+		fwrite(fid, var_T, 'double'); % Cov(Z,Z)
+
+		% the mad file has rotations in NED coordiantes, but
+		% the noisypath file has rotations in ENU coordinates
+		[roll,pitch,yaw] = decomposeRotationMatrix(...
+				convert_NED_to_mat(poses(4:6,i)));
+		fwrite(fid, roll,  'double');
+		fwrite(fid, pitch, 'double');
+		fwrite(fid, yaw,   'double');
+
+		% the covariance matrices for the rotations are also
+		% 'made up' in this context.
+		fwrite(fid, var_R, 'double'); % Cov(roll,  roll)
+		fwrite(fid, 0,     'double'); % Cov(roll,  pitch)
+		fwrite(fid, 0,     'double'); % Cov(roll,  yaw)
+		fwrite(fid, var_R, 'double'); % Cov(pitch, pitch)
+		fwrite(fid, 0,     'double'); % Cov(pitch, yaw)
+		fwrite(fid, var_R, 'double'); % Cov(yaw,   yaw)
+	end
+
+	% clean up
+	fclose(fid);
+end
---
+++ src/matlab/path/read_noisypath.m
@@ -0,0 +1,113 @@
+function path = read_noisypath(filename)
+	% path = read_noisypath(filename)
+	%
+	%	Parses the given .noisypath file and returns 
+	%	a structure containing its info
+	%
+	% arguments:
+	%
+	%	filename -	The file to read and parse
+	%
+	% output:
+	%
+	%	The output structure that represents the data
+	%	in this file.  It contains the following fields:
+	%
+	%		zupts -	List of zupts defined in file
+	%		poses -	List of poses defined in file
+	%
+	%	Each zupt structure has the following fields:
+	%
+	%		start_time -	The start of the zupt interval (sec)
+	%		end_time -	The end of the zupt interval (sec)
+	%
+	%	Each pose structure has the following fields:
+	%
+	%		timestamp -	The timestamp of this pose (sec)
+	%		pos_mean -	The position mean (size: 3x1,
+	%				units: meters)
+	%		pos_cov -	The position covariance matrix
+	%				(size: 3x3)
+	%		rot_mean -	Rotational mean (roll, pitch, yaw),
+	%				all angles are in radians
+	%		rot_cov -	The rotational covariance matrix,
+	%				(size: 3x3)
+	%
+	% author:
+	%
+	%	Eric Turner <elturner@eecs.berkeley.edu>
+	%	May 19th, 2014
+	%
+
+	% attempt to open binary file
+	fid = fopen(filename, 'rb');
+	if(fid < 0)
+		error(['Unable to open file: ', filename]);
+	end
+
+	% read magic number
+	magic = fread(fid, 10, 'char')';
+	if(~strcmp(char(magic), ['noisypath', 0]))
+		keyboard;
+		fclose(fid);
+		error(['Invalid file format.  ', ...
+			'Given file had magic number: ', magic]);
+	end
+
+	% read header information
+	num_zupts = fread(fid, 1, 'uint32');
+	num_poses = fread(fid, 1, 'uint32');
+
+	% read zupt information
+	if(num_zupts == 0)
+		% put empty list
+		path.zupts = [];
+	else
+		% iterate over zupts in file
+		for i = 1:num_zupts
+			path.zupts(i).start_time = fread(fid,1,'double');
+			path.zupts(i).end_time   = fread(fid,1,'double');
+		end
+	end
+
+	% read poses
+	for i = 1:num_poses
+		
+		path.poses(i).timestamp    = fread(fid,1,'double');
+		
+		path.poses(i).pos_mean     = zeros(3,1);
+		path.poses(i).pos_mean(1)  = fread(fid,1,'double');
+		path.poses(i).pos_mean(2)  = fread(fid,1,'double');
+		path.poses(i).pos_mean(3)  = fread(fid,1,'double');
+		
+		path.poses(i).pos_cov      = zeros(3,3);
+		path.poses(i).pos_cov(1,1) = fread(fid,1,'double');
+		path.poses(i).pos_cov(1,2) = fread(fid,1,'double');
+		path.poses(i).pos_cov(1,3) = fread(fid,1,'double');
+		path.poses(i).pos_cov(2,2) = fread(fid,1,'double');
+		path.poses(i).pos_cov(2,3) = fread(fid,1,'double');
+		path.poses(i).pos_cov(3,3) = fread(fid,1,'double');
+		path.poses(i).pos_cov(2,1) = path.poses(i).pos_cov(1,2);
+		path.poses(i).pos_cov(3,1) = path.poses(i).pos_cov(1,3);
+		path.poses(i).pos_cov(3,2) = path.poses(i).pos_cov(2,3);
+
+		path.poses(i).rot_mean     = zeros(3,1);
+		path.poses(i).rot_mean(1)  = fread(fid,1,'double');
+		path.poses(i).rot_mean(2)  = fread(fid,1,'double');
+		path.poses(i).rot_mean(3)  = fread(fid,1,'double');
+		
+		path.poses(i).rot_cov      = zeros(3,3);
+		path.poses(i).rot_cov(1,1) = fread(fid,1,'double');
+		path.poses(i).rot_cov(1,2) = fread(fid,1,'double');
+		path.poses(i).rot_cov(1,3) = fread(fid,1,'double');
+		path.poses(i).rot_cov(2,2) = fread(fid,1,'double');
+		path.poses(i).rot_cov(2,3) = fread(fid,1,'double');
+		path.poses(i).rot_cov(3,3) = fread(fid,1,'double');
+		path.poses(i).rot_cov(2,1) = path.poses(i).rot_cov(1,2);
+		path.poses(i).rot_cov(3,1) = path.poses(i).rot_cov(1,3);
+		path.poses(i).rot_cov(3,2) = path.poses(i).rot_cov(2,3);
+	end
+
+	% cleanup
+	fclose(fid);
+end
