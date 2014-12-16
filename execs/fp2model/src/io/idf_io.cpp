d4a3c589afded8fc5c436a21bd503188088cc2d4
elturner <elturner@eecs.berkeley.edu>
2014-12-16 10:58:59 -0800

    the changes made to the idf export in fp2model in the previous commit cause the walls to not be read by simergy (even though the wall information was not changed at all).  so, changed it back for the sake of sanity

--- execs/fp2model/src/io/idf_io.cpp
+++ execs/fp2model/src/io/idf_io.cpp
@@ -292,12 +292,12 @@ void writefloorandceiling(ofstream& outfile, const building_model_t& bm,
 		<< "    Ceiling,         !- Surface Type" << "\r\n"
 		<< "    Interior Ceiling,!- Construction Name" << "\r\n"
 		<< "    " << zonename << ", !- Zone Name" << "\r\n"
-		<< "    Surface,         !- Outside Boundary Condition"
+		<< "    Outdoors,         !- Outside Boundary Condition"
 			<< "\r\n"
 		<< "    , !- Outside Boundary Condition Object" << "\r\n"
 		<< "    NoSun,           !- Sun Exposure" << "\r\n"
 		<< "    NoWind,          !- Wind Exposure" << "\r\n"
-		<< "    ,             !- View Factor to Ground" << "\r\n"
+		<< "    -1,              !- View Factor to Ground" << "\r\n"
 		<< "    " << n << ",     !- Number of Vertices" << "\r\n";
 
 	/* define geometry of ceiling */
