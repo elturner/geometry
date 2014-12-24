3c85bdefaea4b3987b54cbe7b95c8f349bb42380
elturner <elturner@eecs.berkeley.edu>
2014-12-24 14:50:15 -0800

    modified carve map sandbox matlab script to give a good-looking example distribution when run

--- src/matlab/sandbox/gauss_carve/driver.m
+++ src/matlab/sandbox/gauss_carve/driver.m
@@ -3,8 +3,8 @@ clear all;
 clc;
 
 % make a test scan
-%scan = make_scandist([-7;2],[1 -0.7; -0.7 1],[3;4],[1 0.9; 0.9 1]);
-scan = make_scandist([-7;-8],[1 -0.7; -0.7 1],[15;20],[1 0.9; 0.9 1]);
+scan = make_scandist([-7;2],[0.4 -0.35; -0.35 0.4],[3;4],[1 -0.6; -0.6 1]);
+%scan = make_scandist([-7;-8],[1 -0.7; -0.7 1],[15;20],[1 0.9; 0.9 1]);
 
 % plot the various distributions
 render_point_pdfs(scan);
