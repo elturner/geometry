3fdac04cbcd089d02db300161e122882570c6d90
elturner <elturner@eecs.berkeley.edu>
2015-01-07 16:27:29 -0800

    updated render_path to allow for labeling each pose by a number

--- src/matlab/path/render_path.m
+++ src/matlab/path/render_path.m
@@ -1,4 +1,4 @@
-function [] = render_path(path, color)
+function [] = render_path(path, color, labels)
 	% render_path(path)
 	%
 	%
@@ -18,9 +18,18 @@ function [] = render_path(path, color)
 	hold all;
 	axis equal;
 
+	% plot the path
 	plot(path(1,:), path(2,:), '-xm');
 
-	if(exist('color', 'var'))
+	% check if we should plot labels
+	if(exist('labels', 'var') && length(labels) == length(path))
+		for i = 1:size(path, 2)
+			text(path(1,i), path(2,i), num2str(labels(i)));
+		end
+	end
+
+	% check for stemplot
+	if(exist('color', 'var') && ~isempty(color))
 		
 		% plot 'color' for each pose
 		stem3(path(1,:), path(2,:), color);
