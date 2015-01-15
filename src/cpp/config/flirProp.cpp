a67e471ee51c65624738e401ddeeee82e0a90c3f
Nick Corso <ncorso@eecs.berkeley.edu>
2015-01-14 16:12:55 -0800

    Added the actual flirPorp files

---
+++ src/cpp/config/flirProp.cpp
@@ -0,0 +1,128 @@
+/*
+*	flirProp.cpp
+*
+*	This class acts as a container for all the flir properties
+*	that are passed in via the .bcfg file
+*/
+
+#include "flirProp.h"
+
+using namespace std;
+
+/*
+	flirProp()
+
+	Construnctor.  Sets all the values to be the unitialized versions.
+*/
+flirProp::flirProp() {
+	this->name = "";
+	this->serialNum = "";
+	this->virtualSerialNumber = "";
+	this->configFile = "";
+	this->rToCommon.clear();
+	this->tToCommon.clear();
+}
+
+/*
+	void toRadianMeters()
+
+	This function converts the values from millimeters and degrees to 
+	radians and meters
+*/
+void flirProp::toRadianMeters() {
+	for(size_t i = 0; i < this->rToCommon.size(); i++) {
+		this->rToCommon[i] *= M_PI/180.0;
+	}
+	for(size_t i = 0; i < this->tToCommon.size(); i++) {
+		this->tToCommon[i] *= 1.0/1000.0;
+	}
+	return;
+}
+
+/**
+ * int assign_props(std::map<std::string,std::string>);
+ *
+ * This function will assign the props from the internal string map 
+ * into the internal variables of the class.  The given map is a
+ * mapping from property names to string representations of the
+ * property values.  This is the only user function that should
+ * need to be defined.
+ *
+ * This should return:
+ *		0 - Success
+ *		1 - Missing Property
+ */
+int flirProp::assign_props(std::map<std::string,std::string>& property_map) {
+
+	int ret_code = 0;
+	map<string,string>::iterator it;
+
+	// Extract the string name
+	it = property_map.find("name");
+	if(it == property_map.end()) {
+		ret_code = 1;
+	}
+	else {
+		this->name = it->second;
+	}
+
+	// Extract the string name
+	it = property_map.find("serialNum");
+	if(it == property_map.end()) {
+		ret_code = 1;
+	}
+	else {
+		this->serialNum = it->second;
+	}
+
+	// Extract the string name
+	it = property_map.find("virtualSerialNum");
+	if(it == property_map.end()) {
+		ret_code = 1;
+	}
+	else {
+		this->virtualSerialNumber = it->second;
+	}
+
+	// Extract the config file name
+	it = property_map.find("configFile");
+	if(it == property_map.end()) {
+		ret_code = 1;
+	}
+	else {
+		this->configFile = it->second;
+	}
+	
+	// Extract the rotation to common
+	it = property_map.find("rToCommon");
+	if(it == property_map.end()) {
+		ret_code = 1;
+	}
+	else {
+		this->rToCommon = sensorProp::extract_as_csv_vector<double>(it->second,3);
+	}
+
+	// Extract the translation to common
+	it = property_map.find("tToCommon");
+	if(it == property_map.end()) {
+		ret_code = 1;
+	}
+	else {
+		this->tToCommon = sensorProp::extract_as_csv_vector<double>(it->second,3);
+	}
+
+	return ret_code;
+}
+
+/**
+* virtual std::string type_tag() =0 const;
+*
+* This function returns the type_tag for the particular
+* instance of the sensorProp.  This MUST match the name
+* given to the sensor type in the config XML file.
+* For example this should return "lasers" for the 
+* laser type objects.
+*/
+std::string flirProp::type_tag() const {
+	return "flirs";
+}
---
+++ src/cpp/config/flirProp.h
@@ -0,0 +1,99 @@
+/*
+*	flirProp.h
+*
+*	This class acts as a container for all the flir properties
+*	that are passed in via the .bcfg file
+*/
+
+#ifndef _H_FLIRPROP_
+#define _H_FLIRPROP_
+
+/*
+*	Includes
+*/
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "sensorProp.h"
+
+class flirProp : public sensorProp {
+
+public:
+
+	/********************************************
+	*	Public Data Members
+	********************************************/
+
+	// This is the name given to the camera
+	std::string name;
+
+	// This is the serial number of the camera
+	std::string serialNum;
+
+	// This is the virtual serial number
+	std::string virtualSerialNumber;
+
+	// This is the configuration file to use
+	// for the camera.
+	std::string configFile;
+
+	// This is the euler angle rotations from the camera frame
+	// to a common frame of reference.  It is in degrees.
+	std::vector<double> rToCommon;
+
+	// This is the translation vector between the camera frame
+	// and a common frame of reference.  It is in millimeters.
+	std::vector<double> tToCommon;
+
+	/********************************************
+	*	Public Member Functions
+	********************************************/
+
+	/*
+		flirProp()
+
+		Construnctor.  Sets all the values to be the unitialized versions.
+	*/
+	flirProp();
+	
+	/*
+		void toRadianMeters()
+
+		This function converts the values from millimeters and degrees to 
+		radians and meters
+	*/
+	void toRadianMeters();
+
+	/**
+	 * virtual int assign_props(std::map<std::string,std::string>) =0;
+	 *
+  	 * This function will assign the props from the internal string map 
+	 * into the internal variables of the class.  The given map is a
+	 * mapping from property names to string representations of the
+	 * property values.  This is the only user function that should
+	 * need to be defined.
+  	 *
+  	 * This should return:
+	 *		0 - Success
+	 *		1 - Missing Property
+  	 */
+	 int assign_props(std::map<std::string,std::string>& property_map);
+
+	/**
+	* virtual std::string type_tag() =0 const;
+	*
+	* This function returns the type_tag for the particular
+	* instance of the sensorProp.  This MUST match the name
+	* given to the sensor type in the config XML file.
+	* For example this should return "lasers" for the 
+	* laser type objects.
+	*/
+	std::string type_tag() const;
+
+};
+
+#endif
