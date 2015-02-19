62aafdafca9da753ed6b4b2c5bb619b5a1fefbb7
elturner <elturner@eecs.berkeley.edu>
2015-02-18 17:44:16 -0800

    updated readMapData used by TraceFP to only read in pos info, and ignore the rest.  it will read in pos info faster than before.

--- src/matlab/gui/trace_fp/readMapData.m
+++ src/matlab/gui/trace_fp/readMapData.m
@@ -1,55 +1,33 @@
 function [mapData] = readMapData(mapFileName)
-% [mapData] = readMapData(filename)
-%
-%	Read the specified *.dq file, and
-%	store in the struct mapData
-%
+	% [mapData] = READMAPDATA(filename)
+	%
+	%	Read the specified *.dq file, and
+	%	store in the struct mapData
+	%
 
-fid = fopen(mapFileName);
-if(fid == -1) 
-    error(['unable to read map file: ', mapFileName]);
-end
+	fid = fopen(mapFileName);
+	if(fid == -1) 
+		error(['unable to read map file: ', mapFileName]);
+	end
 
-% Read the header information
-tline = fgets(fid);
-mapData.maxDepth = str2num(tline);
-tline = fgets(fid);
-mapData.halfwidth = str2num(tline);
-tline = fgets(fid);
-mapData.rootNodePosition = str2num(tline)';
-tline = fgets(fid);
+	% Read the header information
+	tline = fgets(fid);
+	mapData.maxDepth = str2num(tline);
+	tline = fgets(fid);
+	mapData.halfwidth = str2num(tline);
+	tline = fgets(fid);
+	mapData.rootNodePosition = str2num(tline)';
+	tline = fgets(fid);
 
-% initialize the fields
-mapData.pos = cell(0);
-mapData.heightRange = cell(0);
-mapData.numPoints = cell(0);
-mapData.poseIdx = cell(0);
+	% initialize the fields
+	pts = textscan(fid, '%f %f %*[^\n]');
+	mapData.pos = [ pts{1}' ; 
+			pts{2}' ];
+				% read first two numbers, which are points
+	mapData.heightRange = []; % we don't care about this
+	mapData.numPoints   = []; % we don't care
+	mapData.poseIdx     = []; % we don't care
 
-% Read the point information
-while(tline ~= -1)
-    
-    % Parse the text string for numbers
-    nline = str2num(tline);
-    if(isempty(nline))
-       continue; 
-    end
-    
-    mapData.pos{end+1} = nline(1:2)';
-    mapData.heightRange{end+1} = nline(3:4)';
-    mapData.numPoints{end+1} = nline(5);
-    if(nline(6) ~= 0)
-        mapData.poseIdx{end+1} = nline(7:end);
-    else
-        mapData.poseIdx{end+1} = []; 
-    end
- 
-    tline = fgets(fid);
-end
-
-% Convert the cells to matricies because fuck cells
-mapData.pos = cell2mat(mapData.pos);
-mapData.heightRange = cell2mat(mapData.heightRange);
-mapData.numPoints = cell2mat(mapData.numPoints);
-
-fclose(fid);
+	% clean up
+	fclose(fid);
 end
