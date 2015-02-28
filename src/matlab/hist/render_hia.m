7808117e94e45bb10f9270d01db02e5ed4ee56e1
elturner <elturner@eecs.berkeley.edu>
2015-02-28 07:03:59 -0800

    added matlab function render_hia

---
+++ src/matlab/hist/render_hia.m
@@ -0,0 +1,35 @@
+function [] = render_hia(hia)
+	% RENDER_HIA(hia)
+	%
+	%	Renders the contents of the hia structure
+	%	to a figure.
+	%
+	%	This structure should be populated by a call
+	%	to read_hia(filename).
+	%
+	% arguments:
+	%
+	%	hia -	The Histogrammed Interior Area (HIA) structure
+	%
+	% author:
+	%
+	%	Written by Eric Turner <elturner@eecs.berkeley.edu>
+	%	Created on February 28th, 2015
+	%
+
+	% prepare the figure
+	hold on;
+	axis equal;
+
+	% prepare the pixels
+	pixels_x = ([-1;-1;1;1] * 0.5 * hia.resolution) ...
+				* ones(1,hia.num_cells) ...
+				+ [1;1;1;1] * hia.centers(:,1)';
+	pixels_y = ([1;-1;-1;1] * 0.5 * hia.resolution) ...
+				* ones(1,hia.num_cells) ...
+				+ [1;1;1;1] * hia.centers(:,2)';
+
+	% render the patches for each pixel
+	patch(pixels_x, pixels_y, hia.open_heights', 'LineStyle', 'None');
+
+end
