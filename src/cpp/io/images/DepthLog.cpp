f5cb655448bf807e03c9b45d4592220449a20356
elturner <elturner@eecs.berkeley.edu>
2015-04-19 14:59:35 -0700

    added Nick Corso's depth and normal log file readers.  These log files are generated by his depth/normal map generator code.

---
+++ src/cpp/io/images/DepthLog.cpp
@@ -0,0 +1,50 @@
+/*
+	DepthLog.cpp
+
+	Provides a class for interfacing with DepthLog files
+*/
+#include "DepthLog.h"
+
+/* includes */
+#include <string>
+#include <vector>
+#include <fstream>
+
+/* namespaces */
+using namespace std;
+
+/* function definitions */
+bool DepthLog::read(const std::string& filename)
+{
+	/* open the file */
+	ifstream inStream(filename.c_str());
+	if(!inStream.is_open())
+		return false;
+
+	/* read in the serial number */
+	inStream >> _name;
+
+	/* read in the number of images */
+	inStream >> _num_images;
+
+	/* read in the K matrix */
+	for(size_t i = 0; i < 9; i++)
+		inStream >> _K[i];
+
+	/* read in the ds factor */
+	inStream >> _dsFactor;
+
+	/* make space for the times and file names */
+	_file_names.resize(_num_images);
+	_timestamps.resize(_num_images);
+
+	/* read them in */
+	for(size_t i = 0; i < _num_images; i++)
+	{
+		inStream >> _timestamps[i];
+		inStream >> _file_names[i];
+	}
+
+	/* return success */
+	return true;
+}
---
+++ src/cpp/io/images/NormalLog.h
@@ -0,0 +1,74 @@
+#ifndef H_NORMALLOG_H
+#define H_NORMALLOG_H
+
+/*
+	NormalLog.h
+
+	Provides a class for interfacing with NormalLog files
+*/
+
+/* includes */
+#include <string>
+#include <vector>
+
+/* the McdFile class */
+class NormalLog
+{
+public:
+
+	/* constructors */
+	NormalLog() {};
+	NormalLog(const std::string& filename)
+		{read(filename);};
+
+	/* read function */
+	bool read(const std::string& filename);
+
+	/* accessors */
+
+	/* gets the name  */
+	inline const std::string& name() const
+		{return _name;};
+
+	/* gets the dsFactor */
+	inline double dsFactor() const
+		{return _dsFactor;};
+
+	/* gets the number of images */
+	inline size_t num_images() const
+		{return _num_images;};
+
+	/* gets the pointer to the K matrix */
+	inline const double * const K() const
+		{return _K;};
+
+	/* gets the ith timestamp */
+	inline double timestamp(size_t i) const
+		{return _timestamps[i];};
+
+	/* gets the ith image name */
+	inline const std::string& file_name(size_t i) const
+		{return _file_names[i];};
+
+private:
+
+	/* holds the name of the camera */
+	std::string _name;
+
+	/* holds the number of images */
+	size_t _num_images;
+
+	/* holds the K matrix */
+	double _K[9];
+
+	/* the scale factor */
+	double _dsFactor;
+
+	/* holds all the timestamps */
+	std::vector<double> _timestamps;
+
+	/* holds all the image file names */
+	std::vector<std::string> _file_names;
+};
+
+#endif
