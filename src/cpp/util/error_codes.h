f78cea190326c2d79e7ec1c83d73305ba2f46a3b
elturner <elturner@eecs.berkeley.edu>
2015-06-30 12:25:58 -0700

    added additional functionality to error_codes, with thanks to Nick Corso.

--- src/cpp/util/error_codes.h
+++ src/cpp/util/error_codes.h
@@ -12,6 +12,7 @@
  */
 
 #include <stdio.h>
+#include <iostream>
 
 /* the following macro will propegate error codes up functions,
  * so that the source of an error is easy to trace. */
@@ -19,5 +20,35 @@
 
 /* Will suppress "unused parameter" warning */
 #define MARK_USED(X)  ((void)(&(X)))
+
+/*------------------------------*/
+/* pretty-print error functions */
+/*------------------------------*/
+
+/* which command used depends on OS */
+#ifdef _WIN32
+	#define PRINTFUN __FUNCSIG__
+#else
+ 	#define PRINTFUN __PRETTY_FUNCTION__
+#endif
+
+/* post errors to cerr and either do nothing, die, or return */
+#define POST_FATAL_ERROR(e, exitCode) { \
+ 	std::cerr << "ERROR IN FILE : " << __FILE__ << '\n' \
+ 		   << "LINE : " << __LINE__ << '\n' \
+ 		   << "FUNCTION : " << PRINTFUN << '\n' \
+ 		   << "MESSAGE : " << e << std::endl << std::endl; \
+ 		   exit(exitCode);}
+#define POST_RETURN_ERROR(e, returnCode) {\
+ 	std::cerr << "ERROR IN FILE : " << __FILE__ << '\n' \
+ 		   << "LINE : " << __LINE__ << '\n' \
+ 		   << "FUNCTION : " << PRINTFUN << '\n' \
+ 		   << "MESSAGE : " << e << std::endl << std::endl; \
+ 		   return(returnCode);}		
+#define POST_ERROR(e) { \
+ std::cerr << "ERROR IN FILE : " << __FILE__ << '\n' \
+ 		   << "LINE : " << __LINE__ << '\n' \
+ 		   << "FUNCTION : " << PRINTFUN << '\n' \
+ 		   << "MESSAGE : " << e << std::endl << std::endl;}
 
 #endif
