7cbfda8e5d745300dc17d547b6a3627b74440bcd
elturner <elturner@eecs.berkeley.edu>
2015-07-08 10:54:13 -0700

    added scanolist_io file format reader, writer, and documentation

---
+++ src/cpp/io/scanorama/scanolist_io.cpp
@@ -0,0 +1,176 @@
+#include "scanolist_io.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+/**
+ * @file    scanolist_io.cpp
+ * @author  Eric Turner <elturner@indoorreality.com>
+ * @brief   Reads and writes scanorama metadata files
+ *
+ * @section DESCRIPTION
+ *
+ * When exporting scanoramas as .ptx files, metadata for each
+ * scanorama pose is also recorded in a scanorama metadata list
+ * file (.scanolist).
+ *
+ * Created July 8, 2015
+ */
+
+using namespace std;
+using namespace scanolist_io;
+
+/* the following constants are used for these files */
+
+#define SCANOLIST_MAGIC_NUMBER  "scanolist"
+
+/*--------------------------*/
+/* function implementations */
+/*--------------------------*/
+
+int scanolist_t::write(const std::string& filename) const
+{
+	ofstream outfile;
+	size_t i, n;
+
+	/* attempt to open file for writing */
+	outfile.open(filename.c_str());
+	if(!(outfile.is_open()))
+	{
+		/* can't open file */
+		cerr << "[scanolist_t::write]\tUnable to open file "
+		     << "for writing: \"" << filename << "\"" << endl;
+		return -1;
+	}
+
+	/* write out header */
+	outfile << SCANOLIST_MAGIC_NUMBER << endl
+		<< this->num_rows << " " << this->num_cols << endl
+		<< this->scano_poses.size() << endl;
+
+	/* write out list of cameras */
+	n = this->camera_names.size();
+	for(i = 0; i < n; i++)
+		outfile << this->camera_names[i] << " ";
+	outfile << endl;
+
+	/* add extra new line at end of header */
+	outfile << endl;
+
+	/* write out all the poses' metadata */
+	n = this->scano_poses.size();
+	for(i = 0; i < n; i++)
+		this->scano_poses[i].print(outfile);
+
+	/* clean up */
+	outfile.close();
+	return 0;
+}
+			
+int scanolist_t::read(const std::string& filename)
+{
+	ifstream infile;
+	string magic, tline, camname;
+	stringstream ss;
+	size_t i, n;
+	int ret;
+
+	/* clear any existing data */
+	this->clear();
+
+	/* attempt to open file for reading */
+	infile.open(filename.c_str());
+	if(!(infile.is_open()))
+	{
+		/* can't open file */
+		cerr << "[scanolist_t::read]\tUnable to find and/or "
+		     << "open file: \"" << filename << "\"" << endl;
+		return -1;
+	}
+
+	/* check for magic number */
+	getline(infile, magic);
+	if(magic.compare(SCANOLIST_MAGIC_NUMBER) != 0)
+	{
+		/* bad file type */
+		cerr << "[scanolist_t::read]\tUnrecognized file format."
+		     << "  Expected \"" << SCANOLIST_MAGIC_NUMBER << "\" "
+		     << "but got \"" << magic << "\"" << endl;
+		infile.close();
+		return -2;
+	}
+
+	/* read header info */
+	infile >> this->num_rows;
+	infile >> this->num_cols;
+	infile >> n; /* number of poses */
+	this->scano_poses.resize(n);
+	getline(infile, tline); /* the list of cameras */
+	ss.str(tline);
+	while(!(ss.eof()))
+	{
+		ss >> camname;
+		if(camname.empty())
+			break; /* no more names */
+		this->camera_names.push_back(camname);
+	}
+
+	/* iterate through poses */
+	for(i = 0; i < n; i++)
+	{
+		/* read next pose */
+		ret = this->scano_poses[i].parse(infile);
+		if(ret)
+		{
+			cerr << "[scanolist_t::read]\tUnable to read "
+			     << "pose #" << i << " (error " << ret
+			     << ") of file: \"" << filename << "\""
+			     << endl;
+			return -2;
+		}
+	}
+
+	/* clean up */
+	infile.close();
+	return 0;
+}
+			
+void scanometa_t::print(std::ostream& os) const
+{
+	/* print the stuff */
+	os << this->index << " " 
+	   << this->timestamp << " " 
+	   << this->filepath << endl;
+}
+			
+int scanometa_t::parse(std::istream& is)
+{
+	stringstream ss;
+	string tline("");
+
+	/* check that file is good */
+	if(is.fail())
+		return -1;
+
+	/* get the next non-empty line */
+	while(tline.empty() && is.good())
+		getline(is, tline);
+	ss.str(tline);
+
+	/* read in the index as an unsigned int */
+	if(!(ss >> this->index))
+		return -2; /* no value */
+
+	/* read timestamp as a double */
+	if(!(ss >> this->timestamp))
+		return -3; /* no value */
+
+	/* read file path */
+	if(!(ss >> this->filepath))
+		return -4; /* no value */
+
+	/* success */
+	return 0;
+}
---
+++ src/cpp/io/scanorama/scanolist_io.h
@@ -0,0 +1,285 @@
+#ifndef SCANOLIST_IO_H
+#define SCANOLIST_IO_H
+
+/**
+ * @file    scanolist_io.h
+ * @author  Eric Turner <elturner@indoorreality.com>
+ * @brief   Reads and writes scanorama metadata files
+ *
+ * @section DESCRIPTION
+ *
+ * When exporting scanoramas as .ptx files, metadata for each
+ * scanorama pose is also recorded in a scanorama metadata list
+ * file (.scanolist).
+ *
+ * Created July 8, 2015
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+/**
+ * The scanolist_io namespace contains all classes used for i/o
+ */
+namespace scanolist_io
+{
+	/* the following classes are defined in this namespace */
+	class scanolist_t;
+	class scanometa_t;
+
+	/**
+	 * The scanolist_t class is used to read and write .scanolist
+	 * files.
+	 *
+	 * It contains some header information and a list of metadata
+	 * about each scanorama pose.
+	 */
+	class scanolist_t
+	{
+		/* parameters */
+		private:
+
+			/**
+			 * The following is the list of cameras
+			 * that were used to color these
+			 * scanoramas.
+			 */
+			std::vector<std::string> camera_names;
+
+			/**
+			 * The following are the dimensions of the
+			 * exported scanoramas, (num_rows x num_cols).
+			 *
+			 * Each pixel is represented by a 3D point
+			 * and a color within a scanorama.
+			 *
+			 * The scanoramas are stored in column-major
+			 * order.
+			 */
+			size_t num_rows;
+			size_t num_cols;
+
+			/**
+			 * The following is the list of scanorama poses
+			 * generated for this dataset.
+			 */
+			std::vector<scanometa_t> scano_poses;
+
+		/* functions */
+		public:
+
+			/*--------------*/
+			/* constructors */
+			/*--------------*/
+
+			/**
+			 * Constructs empty scanolist
+			 */
+			scanolist_t() : 
+				camera_names(), 
+				num_rows(0),
+				num_cols(0),
+				scano_poses()
+			{};
+
+
+			/*----------------*/
+			/* initialization */
+			/*----------------*/
+
+			/**
+			 * Clears all info in this object
+			 */
+			inline void clear()
+			{
+				this->camera_names.clear();
+				this->num_rows = 0;
+				this->num_cols = 0;
+				this->scano_poses.clear();
+			};
+
+			/**
+			 * Sets the dimensions of each scanorama
+			 *
+			 * @param nrows   The number of rows in each
+			 *                scanorama
+			 * @param ncols   The number of columns in each
+			 *                scanorama
+			 */
+			inline void set_dims(size_t nrows, size_t ncols)
+			{
+				this->num_rows = nrows;
+				this->num_cols = ncols;
+			};
+
+			/**
+			 * Adds a camera name
+			 *
+			 * The camera names indicate which cameras
+			 * were used to color the scanoramas
+			 * in this list.
+			 *
+			 * @param name   The name of the camera to
+			 *               add.
+			 */
+			inline void add_camera(const std::string& name)
+			{ this->camera_names.push_back(name); };
+
+			/**
+			 * Adds metadata about the next scanorama pose
+			 *
+			 * Will add this metadata to the end of the
+			 * list of poses.
+			 *
+			 * @param p   The pose information to add
+			 */
+			inline void add(const scanometa_t& p)
+			{ this->scano_poses.push_back(p); };
+
+			/*-----*/
+			/* i/o */
+			/*-----*/
+
+			/**
+			 * Writes this data to the specified .scanolist
+			 * file.
+			 *
+			 * @param filename   The .scanolist file to write to
+			 *
+			 * @return    Returns zero on success, non-zero
+			 *            on failure.
+			 */
+			int write(const std::string& filename) const;
+
+			/**
+			 * Reads data from specified scanolist file.
+			 *
+			 * All data stored previously will be destroyed
+			 * and replaced by data from given file.
+			 *
+			 * @param filename  The .scanolist file to read
+			 *
+			 * @return    Returns zero on success, non-zero
+			 *            on failure.
+			 */
+			int read(const std::string& filename);
+	};
+	
+	/**
+	 * The scanometa_t class stores metadata for a single
+	 * scanorama pose.
+	 */
+	class scanometa_t
+	{
+		/* parameters */
+		public:
+
+			/**
+			 * The index of this pose
+			 */
+			size_t index;
+
+			/**
+			 * The timestamp of this pose
+			 */
+			double timestamp;
+
+			/**
+			 * The path to the scanorama file
+			 */
+			std::string filepath;
+
+		/* functions */
+		public:
+
+			/*--------------*/
+			/* constructors */
+			/*--------------*/
+
+			/**
+			 * Constructs default, invalid metadata
+			 */
+			scanometa_t(): index(0), timestamp(0), filepath("")
+			{};
+
+			/**
+			 * Constructs metadata from specified parameters
+			 *
+			 * @param i    The index of this pose
+			 * @param t    The timestamp of this pose
+			 * @param f    The filepath to scanorama file
+			 */
+			scanometa_t(size_t i,double t,const std::string& f)
+				: index(i), timestamp(t), filepath(f)
+			{};
+
+			/**
+			 * Constructs by copying from specified scanometa_t
+			 * object
+			 *
+			 * @param other   The object to copy
+			 */
+			scanometa_t(const scanometa_t& other)
+				: index(other.index),
+				  timestamp(other.timestamp),
+				  filepath(other.filepath)
+			{};
+
+			/*-----*/
+			/* i/o */
+			/*-----*/
+
+			/**
+			 * Prints this metadata info to the specified
+			 * text stream.
+			 *
+			 * Will print the data in the format needed
+			 * for a .scanolist file to the specified text
+			 * stream.  This will generate one new line
+			 * in the file stream.
+			 *
+			 * @param os   The output stream to write to
+			 */
+			void print(std::ostream& os) const;
+
+			/**
+			 * Parses the next metadata line from the
+			 * specified input stream
+			 *
+			 * Will attempt to parse the next line as a
+			 * scanometa_t.
+			 *
+			 * @param is   The input stream to parse
+			 *
+			 * @return     Returns zero on success, non-zero
+			 *             on failure.
+			 */
+			int parse(std::istream& is);
+
+			/*-----------*/
+			/* operators */
+			/*-----------*/
+
+			/**
+			 * Copies values from specified other scanometa_t
+			 *
+			 * @param other   The scanometa_t to copy
+			 *
+			 * @return    Returns copy of modified structure
+			 */
+			inline scanometa_t& operator = (
+					const scanometa_t& other)
+			{
+				/* copy values */
+				this->index     = other.index;
+				this->timestamp = other.timestamp;
+				this->filepath  = other.filepath;
+
+				/* return the result */
+				return *(this);
+			};
+	};
+}
+
+#endif
